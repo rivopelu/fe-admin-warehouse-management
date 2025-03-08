f7ebc74144e0229a81d99610d6d98d177fb714fb
rivopelu <rivopelu12@gmail.com>
2025-03-08 18:16:40 +0700

    FEAT:setup animation

--- src/components/molecules/Base.tsx
+++ src/components/molecules/Base.tsx
@@ -2,25 +2,53 @@ import { PAGE_TYPE_ENUM } from '../../enums/page-type-enum.ts';
 import { ReactNode } from 'react';
 import Sidebar from './Sidebar.tsx';
 import TopBar from './TopBar.tsx';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 export function Base(props: IProps) {
+  const location = useLocation(); // Dapatkan path saat ini
+
   function checkPage() {
     switch (props.type) {
       case PAGE_TYPE_ENUM.PRIMARY:
         return (
-          <div className={'flex  min-h-screen'}>
+          <div className="flex min-h-screen">
             <Sidebar />
-            <div className={'flex-1'}>
+            <div className="flex-1 flex flex-col">
               <TopBar />
-              <div className={'flex-1'}>{props.children}</div>
+              <AnimatePresence mode="wait">
+                <motion.div
+                  key={location.pathname} // Pastikan animasi berjalan saat route berubah
+                  className="flex-1"
+                  initial={{ opacity: 0, y: 5 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -5 }}
+                  transition={{ duration: 0.2, ease: 'easeInOut' }}
+                >
+                  {props.children}
+                </motion.div>
+              </AnimatePresence>
             </div>
           </div>
         );
       default:
-        return <>{props.children}</>;
+        return (
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: 5 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -5 }}
+              transition={{ duration: 0.2, ease: 'easeInOut' }}
+            >
+              {props.children}
+            </motion.div>
+          </AnimatePresence>
+        );
     }
   }
-  return <main className={'bg-base-background min-h-screen'}>{checkPage()}</main>;
+
+  return <main className="bg-base-background min-h-screen">{checkPage()}</main>;
 }
 
 interface IProps {
