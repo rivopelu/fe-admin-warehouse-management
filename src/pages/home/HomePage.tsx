0e7f221ba52272b009dfcf716b2d84c90a436f03
rivopelu <rivopelu12@gmail.com>
2025-03-08 19:39:15 +0700

    FEAT:clear console

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,29 +1,11 @@
 import Button from '../../components/atoms/Button.tsx';
 import { useAuth } from '../../hooks/useAuth.ts';
-import { HttpService } from '../../services/http.service.ts';
-import { useEffect } from 'react';
-import { ENDPOINT } from '../../constants/endpoint.ts';
-import { BaseResponse } from '../../types/data/IResModel.ts';
-import { IResGetMe } from '../../types/data/IResGetMe.ts';
-import ErrorService from '../../services/error.service.ts';
 import PageContainer from '../../components/atoms/PageContainer.tsx';
 import { PageTitle } from '../../components/atoms/PageTitle.tsx';
 import { t } from 'i18next';
 
 export default function HomePage() {
   const auth = useAuth();
-  const httpService = new HttpService();
-  const errorService = new ErrorService();
-  useEffect(() => {
-    httpService
-      .GET(ENDPOINT.GET_ME())
-      .then((e: BaseResponse<IResGetMe>) => {
-        console.info(e);
-      })
-      .catch((e) => {
-        errorService.fetchApiError(e);
-      });
-  }, []);
 
   return (
     <div>
